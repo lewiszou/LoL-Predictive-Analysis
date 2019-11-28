56b4b495036b2ab9a9f9b5327afba00bef334f71
lewiszou <lzou@college.harvard.edu>
2019-11-28 10:57:55 -0500

    Finished the introduction page — moving onto doing real analysis!

--- LoL-Predictive-Analysis/app.R
+++ LoL-Predictive-Analysis/app.R
@@ -1,7 +1,20 @@
 library(shinythemes)
+library(tidyverse)
 
 worlds_18 <- readRDS("clean-data/worlds_2018.rds")
 definitions <- readRDS("clean-data/definitions.rds")
+
+# Adjusting the Dataset for the interactive graphs in the Intro page
+dataset <- worlds_18 %>% 
+  filter(position == "Team") %>%
+  select(totalgold, team, position, teamkills, teamdragkills, teamtowerkills, teamtowerkills)
+x_dataset <- worlds_18 %>%
+  filter(position == "Team") %>%
+  select(totalgold, goldat10, goldat15)
+y_dataset <- dataset %>%
+  select(teamkills, teamdragkills, teamtowerkills, teamtowerkills)
+position_dataset <- dataset %>%
+  select(position)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("flatly"),
@@ -59,7 +72,24 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                             style="display: block; margin-left: auto; margin-right: auto;", 
                             height = "475px"),
                         h5("2018 World Championships Finals", align = "center"),
-                        HTML(readLines('worlds_intro.html'))
+                        HTML(readLines('worlds_intro.html')),
+                        
+                        # Dataset Table for Interaction
+                        fluidRow(
+                          column(3,
+                                 h4("Gold Explorer"),
+                                 sliderInput('sampleSize', 'Sample Size',
+                                             min=1, max=nrow(dataset),
+                                             value=min(1000, nrow(dataset)),
+                                             step=10, round=0)),
+                          column(4, offset = 1,
+                                 selectInput('x', 'X', names(x_dataset)),
+                                 selectInput('y', 'Y', names(y_dataset), names(y_dataset)[[2]])),
+                          column(4,
+                                 selectInput('color', 'Color', c('None', names(dataset))),
+                                 checkboxInput('jitter', 'Jitter'),
+                                 checkboxInput('smooth', 'Smooth'))),
+                          plotOutput('interactive_intro')
                ),
                
                tabPanel("2018 Worlds: Deep Dive"),
@@ -75,8 +105,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 server <- function(input, output) {
 
     
-  # About Section
-
+  # Dataset Page — Datatable
   output$table <- DT::renderDataTable(DT::datatable({
     data <- worlds_18
     if (input$team != "All") {
@@ -91,9 +120,27 @@ server <- function(input, output) {
     data
     }))
   
-  # Dataset
-  
+  # Dataset Page — Definitions
   output$select <- renderText({input$select})
+  
+  # Intro Page — Graphing
+  dataset <- reactive({
+    worlds_18[sample(nrow(worlds_18), input$sampleSize),]
+  })
+  output$interactive_intro <- renderPlot({
+    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+    if (input$color != 'None')
+      p <- p + aes_string(color=input$color)
+    if (input$jitter)
+      p <- p + geom_jitter()
+    if (input$smooth)
+      p <- p + geom_smooth(method = "lm")
+    print(p)
+  })
+  
+  
+  
+  
   
 }
 
